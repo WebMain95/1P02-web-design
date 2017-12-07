d27203ffcbb17b78c7cc9534fd907807cdef05e3
WebMain95 <32036107+WebMain95@users.noreply.github.com>
2017-12-07 00:44:34 -0500

    Delete scripts.js

--- ex3/scripts.js
+++
@@ -1,8 +0,0 @@
-function eq(){
-
-  var number2 = document.getElementById("number2").value;
-  var number1 = document.getElementById("number1").value;
-
-  var final= Number(number2) + Number(number2);
-  document.getElementById("output").innerHTML = final;
-}
